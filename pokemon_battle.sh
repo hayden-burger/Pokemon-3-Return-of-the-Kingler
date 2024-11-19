056a836e6905a942e94e41c96ab4f02a0fd7b770
Corinne Desroches <corinne.desroches@nps.edu>
2024-11-19 14:51:16 -0800

    New bash to run pokemon_script.py

--- pokemon_battle.sh
+++ pokemon_battle.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #SBATCH --job-name HEB_pokemonbattle
-#SBATCH --array=1-3
+#SBATCH --array=1-4
 #SBATCH --output=Output_data_files/out_battle_%a.txt
 
 . /etc/profile
@@ -9,5 +9,5 @@ module load lang/python
 ## load the comp3 virtual environment
 source /smallwork/$USER/comp3/bin/activate
 
-python -m pip install --quiet openpyxl
-python run_script.py ${SLURM_ARRAY_TASK_ID} 
+## arguments:  team number, number of runs
+python pokemon_script.py ${SLURM_ARRAY_TASK_ID} 10
